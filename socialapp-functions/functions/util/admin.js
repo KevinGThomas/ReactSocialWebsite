585d98acb8c5bd8822a62ffb5e9193b71f8b063f
Kevin George Thomas <kevin.thomas@lsz.ads.valuelabs.net>
2020-01-27 11:44:23 +0530

    Added Redux

---
+++ socialapp-functions/functions/util/admin.js
@@ -0,0 +1,12 @@
+const admin = require("firebase-admin")
+
+admin.initializeApp({
+  credential: admin.credential.cert(require("./admin.json")),
+  storageBucket: "socialapp-2b1be.appspot.com"
+})
+
+const db = admin.firestore()
+
+module.exports = { admin, db }
+
+//{credential: admin.credential.cert(require("../../keys/admin.json"))}
---
+++ socialapp-functions/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyCQ8qtkqRnXFCR2ebDiLroz7bbcAYuv9zc",
+  authDomain: "socialapp-2b1be.firebaseapp.com",
+  databaseURL: "https://socialapp-2b1be.firebaseio.com",
+  projectId: "socialapp-2b1be",
+  storageBucket: "socialapp-2b1be.appspot.com",
+  messagingSenderId: "225918259567",
+  appId: "1:225918259567:web:cd53f4bf9681568f65989a"
+}
