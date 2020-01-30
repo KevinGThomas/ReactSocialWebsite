49f0aa9e95927aa5bacddc76cebb3cfe7105167b
Kevin George Thomas <kevin.thomas@lsz.ads.valuelabs.net>
2020-01-30 12:15:13 +0530

    Updates

---
+++ precache-manifest.7fb46067ba911af51c4ea825589cad1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583503006776f0e19d7030ce0f13214f",
+    "url": "/ReactSocialWebsite/index.html"
+  },
+  {
+    "revision": "a58f420e55603ab83bb3",
+    "url": "/ReactSocialWebsite/static/css/main.92a846c7.chunk.css"
+  },
+  {
+    "revision": "082a0b6786cbe40bc7b9",
+    "url": "/ReactSocialWebsite/static/js/2.07cc9e80.chunk.js"
+  },
+  {
+    "revision": "3d416322b0ec4a5a91d5be4afe7c848c",
+    "url": "/ReactSocialWebsite/static/js/2.07cc9e80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a58f420e55603ab83bb3",
+    "url": "/ReactSocialWebsite/static/js/main.8f83ecc8.chunk.js"
+  },
+  {
+    "revision": "a332c5ab097a3d617efa",
+    "url": "/ReactSocialWebsite/static/js/runtime-main.7ce4de9b.js"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/ReactSocialWebsite/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "522efa4696c33f47f74192038b6d9590",
+    "url": "/ReactSocialWebsite/static/media/site_logo.522efa46.png"
+  }
+]);
